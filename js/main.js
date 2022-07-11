e682eb57530cb7ba561185b54f970e4aba9a604c
finiko-izh <d.novoselov1993@gmail.com>
2022-07-11 22:36:15 +0400

    P104

--- js/main.js
+++ js/main.js
@@ -776,3 +776,8 @@ if (find('.field-range__slide--input')) {
         setValue(this.value)
     });
 }
+
+
+document.addEventListener('touchmove', function(event) {
+    if (event.scale !== 1) { event.preventDefault(); }
+}, { passive: false });
