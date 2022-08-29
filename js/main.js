a3939337f2614a34449778d07bb8d0affb5ec6a1
finiko-izh <d.novoselov1993@gmail.com>
2022-08-29 14:22:03 +0400

    fixed chat

--- js/main.js
+++ js/main.js
@@ -425,7 +425,7 @@ class formSubmit {
             case 'keyup':
                 if (e.target.id === 'birthday' && (e.key !== "Backspace" || e.keyCode !== 8)) {
                     //if (e.target.value.length >= 10) return;
-                    alert(e.key, e.keyCode);
+                    alert(e.key);
                     e.target.value = maskDate(e.target.value)
                 }
                 break;
